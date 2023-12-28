7d13d5d19e170381f1d11f6dad148554e20cb9ee
silverblade34 <mpacheco.tacay@gmail.com>
2023-12-28 17:43:55 -0500

    uso de loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
 import { CustomersComponent } from "./components/customers/customers.component";
+import { CommentsComponent } from './components/comments/comments.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [CommonModule, RouterOutlet, UserComponent, CustomersComponent]
+  imports: [CommonModule, RouterOutlet, UserComponent, CustomersComponent, CommentsComponent]
 })
 export class AppComponent {
   title = 'ElectronicBilling';
