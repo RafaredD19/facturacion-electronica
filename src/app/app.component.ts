f5693edd85ca23ebdea1dda5b936c9800d4834d3
silverblade34 <mpacheco.tacay@gmail.com>
2023-12-28 17:25:16 -0500

    se esta practicando la logica basica de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './components/user/user.component';
+import { CustomersComponent } from "./components/customers/customers.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [CommonModule, RouterOutlet, UserComponent, CustomersComponent]
 })
 export class AppComponent {
   title = 'ElectronicBilling';
+  city: string = "La Lema"
+  userOcupation: string = "Dveloper"
+  childsMessage: string = ""
+
+  receiveEmition($event: string): void {
+    this.childsMessage = $event
+  }
 }
